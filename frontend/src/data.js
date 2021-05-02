20dd814114a4cf1cb4c75555b9808087bcaeaf18
krishnakumartp <krishnakumartp@yahoo.com>
2021-05-02 15:25:27 +0530

    First update

---
+++ frontend/src/data.js
@@ -0,0 +1,79 @@
+const data ={
+    products : [
+        {
+            id: '1',
+            name : 'Ganapathy',
+            catagory : 'Paintings',
+            image: './images/BGM254-Ganapathi.jpeg',
+            price:120,
+            brand:'GURU',
+            rating:4.5,
+            numReviews:10,
+            description:'This is India god Ganapathi canvas printing',
+        },
+
+        {
+            id: '2',
+            name : 'Ganapathy',
+            catagory : 'Paintings',
+            image: './images/BGM254-Ganapathi.jpeg',
+            price:120,
+            brand:'GURU',
+            rating:4.5,
+            numReviews:10,
+            description:'This is India god Ganapathi canvas printing',
+        },
+        
+        {
+            id: '3',
+            name : 'Ganapathy',
+            catagory : 'Paintings',
+            image: './images/BGM254-Ganapathi.jpeg',
+            price:120,
+            brand:'GURU',
+            rating:4.5,
+            numReviews:10,
+            description:'This is India god Ganapathi canvas printing',
+        },
+        
+        {
+            id: '4',
+            name : 'Ganapathy',
+            catagory : 'Paintings',
+            image: './images/BGM254-Ganapathi.jpeg',
+            price:120,
+            brand:'GURU',
+            rating:4.5,
+            numReviews:10,
+            description:'This is India god Ganapathi canvas printing',
+        },
+        
+        {
+            id: '5',
+            name : 'Ganapathy',
+            catagory : 'Paintings',
+            image: './images/BGM254-Ganapathi.jpeg',
+            price:120,
+            brand:'GURU',
+            rating:4.5,
+            numReviews:10,
+            description:'This is India god Ganapathi canvas printing',
+        },
+        
+        {
+            id: '6',
+            name : 'Ganapathy',
+            catagory : 'Paintings',
+            image: './images/BGM254-Ganapathi.jpeg',
+            price:60,
+            brand:'GURU',
+            rating:4.5,
+            numReviews:10,
+            description:'This is India god Ganapathi canvas printing',
+        },
+        
+        
+
+    ],
+};
+export default data;
